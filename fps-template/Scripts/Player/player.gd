dff69faca8eff754da4ff97f27a60f59f8412fdc
Sytsn <jwclaus12@gmail.com>
2026-02-08 13:28:14 -0500

    clean player script and make it worclean player script Please enter the commit message for your changes. Lines starting

--- fps-template/Scripts/Player/player.gd
+++ fps-template/Scripts/Player/player.gd
@@ -1,18 +1,12 @@
 class_name Player extends CharacterBody3D
 
-var mouse_sens := 0.001
 
-@export var speed := 10.0
-@export var gravity := -20.0
-@export var jump_impulse := 10.0
-
+@export var player_res: PlayerRes
 @export var neck: Node3D
 @export var camera: Camera3D
+@export var is_multiplayer: bool = true
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 7
-
-@onready var is_paused = false
+var is_paused = false
 
 
 func _enter_tree() -> void:
@@ -21,28 +15,22 @@ func _enter_tree() -> void:
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func move_player(input_dir: Vector2):
+	var direction := (neck.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * player_res.move_speed
+		velocity.z = direction.z * player_res.move_speed 
+	else:
+		velocity.x = move_toward(velocity.x, 0, player_res.move_speed)
+		velocity.z = move_toward(velocity.z, 0, player_res.move_speed)
 	
-	
-func _process(delta: float) -> void:
-	if Input.is_action_just_released("ui_cancel") and not is_paused:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		is_paused = true
-	elif Input.is_action_just_released("ui_cancel") and is_paused:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		is_paused = false
+	move_and_slide()
 
 
-func _physics_process(_delta: float) -> void:
-	pass
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		neck.rotate_y(-event.relative.x * mouse_sens)
-		camera.rotate_x(-event.relative.y * mouse_sens)
-		
-		camera.rotation.x = clamp(
-		camera.rotation.x,
-		deg_to_rad(-90),
-		deg_to_rad(90)
-	)
+func stop_player(delta: float):
+	velocity.y += player_res.gravity * delta
+	velocity.x = move_toward(velocity.x, 0, player_res.move_speed)
+	velocity.z = move_toward(velocity.z, 0, player_res.move_speed)
+	move_and_slide()
