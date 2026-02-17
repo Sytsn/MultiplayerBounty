a57d6d51617fc1ca58a5dfeb98a8ec3c0689dc52
Sytsn <jwclaus12@gmail.com>
2026-02-17 18:24:43 -0500

    move action check to weapon manager

--- fps-template/Scripts/States/Weapon/weapon_state.gd
+++ fps-template/Scripts/States/Weapon/weapon_state.gd
@@ -9,3 +9,12 @@ func _ready() -> void:
 	await owner.ready
 	weapon_manager = owner as WeaponManager
 	assert(weapon_manager != null, "The PlayerState state type must be used only in the player scene. It needs the owner to be a Player node.")
+
+
+func update(delta: float) -> void:
+	action_inputs()
+
+
+func action_inputs():
+	if Input.is_action_just_pressed("action_1"):
+		weapon_manager.weapon_action()
